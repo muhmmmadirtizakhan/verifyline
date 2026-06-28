6ac1b2b2a66d7b79d6b535e785dc43a69e1b2ab7
MUHAMMAD IRTIZA KHAN <irtizakhan844@gmail.com>
2026-06-29 04:35:17 +0500

    Update server.js

--- server.js
+++ server.js
@@ -893,10 +893,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/style.css', (req, res) => {
+  res.setHeader('Content-Type', 'text/css');
+  res.sendFile(path.join(__dirname, 'style.css'));
+});
+
+app.get('/script.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(path.join(__dirname, 'script.js'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
-
 // ============================================================
 // ===== START SERVER =====
 // ============================================================
